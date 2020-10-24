0dc2c7556e98ea4e8ad2d599617d184e2f627785
ayushk0412 <52037690+ayushk0412@users.noreply.github.com>
2020-10-25 01:36:03 +0530

    Update views.py

--- spaceapp/views.py
+++ spaceapp/views.py
@@ -61,76 +61,8 @@ def contactus(request):
 
 @login_required
 def quiz_score(request):
-    quiz = QuizDB_Live.objects.all()
-    score = 0
-    choice = [request.POST.get('1', ""),
-              request.POST.get('2', ""),
-              request.POST.get('3', ""),
-              request.POST.get('4', ""),
-              request.POST.get('5', ""),
-              request.POST.get('6', ""),
-              request.POST.get('7', ""),
-              request.POST.get('8', ""),
-              request.POST.get('9', ""),
-              request.POST.get('10', ""),
-              ]
-    answer = []
-    for i in quiz:
-        answer.append(i.answer)
-    for i in range(len(choice)):
-        if i >= 8:
-            if answer[i] == choice[i]:
-                score += 6
-            else:
-                score += 0
-        else:
-            if answer[i] == choice[i]:
-                score += 2
-            else:
-                score += 0
-    print(choice)
-    print(score)
-    for i in range(len(choice)):
-        print(choice[i])
-    """current = request.user
-    currentprofile = current.profile.id
-    b = Profile.objects.get(pk=currentprofile)
-    current_points = b.points
-    print('current points', current_points)
-    a = Profile.objects.filter(pk=currentprofile).update(
-        points=current_points+2)"""
-    currentUserName = request.user
-    currentprofile = currentUserName.profile.id
-    profile_obj = Profile.objects.get(pk=currentprofile)
-    UpdatedScore = Profile.objects.filter(pk=currentprofile).update(
-        points=(profile_obj.points)+score)
-    print('score:', profile_obj.points)
-    return render(request, 'quiz_result.html', {'score': score, 'choice': choice, 'quiz': quiz})
-
-
-@login_required
-def kids(request):
-    return render(request, 'kidszone.html')
-
-
-@login_required
-def quiz_video(request):
-    return render(request, 'quiz_video.html')
-
-
-@login_required
-def visual_novel_story(request):
-    return render(request, 'VN/story/index.html')
-
-
-@login_required
-def visual_novel_quiz1(request):
-    return render(request, 'VN/quiz1/index.html')
-
-
-@login_required
-def visual_novel_quiz2(request):
-    return render(request, 'VN/quiz2/index.html')
+    
+   #TO GET THE FULL CODE, SEND ME A MAIL AT ayushk0412@gmail.com, WITH THE SUBJECT "CODE REQUIRED"
 
 
 def about(request):
